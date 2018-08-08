d28e820ff8905f21593fa5ef2700fbdb6e7f2253
Arpan Srivastava (Ubuntu) <arpansri98@gmail.com>
2018-08-08 09:10:07 +0530

    Completed the Chat UI and restructured the folders.

---
+++ app/src/main/java/com/bhumika/labs/brobot/model/Message.java
@@ -0,0 +1,40 @@
+package com.bhumika.labs.brobot.model;
+
+public class Message {
+
+    private String message;
+    private String sender;
+    private long createdAt;
+
+    public Message(String message, String sender, long createdAt) {
+        this.message = message;
+        this.sender = sender;
+        this.createdAt = createdAt;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public long getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(long createdAt) {
+        this.createdAt = createdAt;
+    }
+
+
+}
